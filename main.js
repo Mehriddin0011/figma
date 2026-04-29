3fca2c03086599a72b460629d2f12767db59473a
Mehriddin0011 <bdjdbdbxhx759@gmail.com>
2026-04-29 10:39:54 +0500

    forst add html,css,js

--- main.js
+++ main.js
@@ -17,3 +17,32 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+let carouselInner = document.querySelector(".carousel__inner")
+let dots = document.querySelectorAll(".dot")
+let current = 0
+
+function changeSlide() {
+  carouselInner.style.transform = `translateX(-${current * 100}%)`
+
+  dots.forEach(function (dot) {
+    dot.classList.remove("active")
+  })
+
+  dots[current].classList.add("active");
+}
+
+dots.forEach(function (dot, index){
+  dot.addEventListener("click", function () {
+    current = index
+    changeSlide()
+  })
+})
+
+setInterval(function (){
+  current++;
+  
+  if(current === dots.length){
+    current = 0;
+  }
+  changeSlide()
+}, 3000) ;
